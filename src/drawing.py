cd83b9ab53beee4a83eafe4625316d4e8c7b863b
francisco ibarrola <fibarrola@sinc.unl.edu.ar>
2022-03-14 14:47:13 +1100

    changed svg parser

--- src/drawing.py
+++ src/drawing.py
@@ -1,7 +1,6 @@
 import torch
 from svgpathtools import svg2paths
 from xml.dom import minidom
-from svgelements import *
 
 
 class DrawingPath():
@@ -18,56 +17,95 @@ def get_width(line):
     width = torch.tensor(width)
 
 def get_drawing_paths(path_to_svg_file):
-    f =  open(path_to_svg_file, 'r')
-    lines = f.readlines()
-    f.close()
     path_list = []
-    
     paths, attributes = svg2paths(path_to_svg_file)
+    
+    for att in attributes:
+        style = att['style'].split(';')
+        for x in style:
+            if len(x) >= 13:
+                if x[:13] == 'stroke-width:':
+                    width = float(x[13:])
+            if len(x) >= 15:
+                if x[:15] == 'stroke-opacity:':
+                    opacity = float(x[15:])
+            if len(x) >= 8:
+                if x[:8] == 'stroke:#':
+                    hex_code = x[8:]
+                    color = list(int(hex_code[i:i+2], 16)/255 for i in (0, 2, 4))
+        color.append(opacity)
+        color = torch.tensor(color)
+        width = torch.tensor(width)
+
+        try:
+            [x_a, x_b] = att['d'].split('c')
+        except:
+            [x_a, x_b] = att['d'].split('C')
+        x0 = [float(x) for x in x_a[2:].split(',')]
+        points = [xx.split(',') for xx in x_b[1:].split(' ')]
+        points = [[float(x), float(y)] for [x,y] in points] 
+        path = [x0]+points
+        num_segments = len(path)//3
+        path = torch.tensor(path)
+        v0 = torch.tensor([0,0])
+        for k in range(path.size(0)):
+            path[k,:] += v0
+            if k%3 == 0:
+                v0 = path[k,:]
 
     
+        path_list.append(DrawingPath(path, color, width, num_segments))
 
-    aSDAS
-
-    for l, line in enumerate(lines):
-        if len(line)<9:
-            continue
-        if line[:9] == '    <path':
-            for k in range(1,4):
-                if lines[l+k][7:12] == 'style':
-                    prop_line = lines[l+k]
-                elif lines[l+k][7] == 'd':
-                    path_line = lines[l+k]
-            
-            # Get color
-            color_idx_0 = prop_line.find('stroke:#')
-            color_idx_1 = prop_line[color_idx_0+8:].find(';')
-            opacity_idx_0 = prop_line.find('stroke-opacity:')
-            opacity_idx_1 = min( [prop_line[opacity_idx_0+15:].find('"'), prop_line[opacity_idx_0+15:].find(';')])
-            hex = prop_line[color_idx_0+8:color_idx_0+8+color_idx_1]
-            color = list(int(hex[i:i+2], 16)/255 for i in (0, 2, 4))
-            color.append(float(prop_line[opacity_idx_0+15:opacity_idx_0+15+opacity_idx_1]))
-            color = torch.tensor(color)
-            # Get width
-            width_idx_0 = prop_line.find('stroke-width:')
-            width_idx_1 = prop_line[width_idx_0+13:].find(';')
-            width = float(prop_line[width_idx_0+13:width_idx_0+13+width_idx_1])
-            width = torch.tensor(width)
-            # Get path
-            idx0 = path_line.find('m')
-            idx1 = path_line.find('c')
-            path = [[float(x) for x in path_line[idx0+2:idx1-1].split(',')]]
-            linedata = path_line[idx1+2:-2].split(' ')
-            for k in range(len(linedata)):
-                path.append([float(x) for x in linedata[k].split(',')])
-            num_segments = len(path)//3
-            path = torch.tensor(path)
-            v0 = torch.tensor([0,0])
-            for k in range(path.size(0)):
-                path[k,:] += v0
-                if k%3 == 0:
-                    v0 = path[k,:]
-            
-            path_list.append(DrawingPath(path, color, width, num_segments))
-
+    # print(path, color, width, num_segments)    
     return path_list
+
+# def get_drawing_paths(path_to_svg_file):
+    # f =  open(path_to_svg_file, 'r')
+    # lines = f.readlines()
+    # f.close()
+    # path_list = []
+    
+    # for l, line in enumerate(lines):
+    #     if len(line)<9:
+    #         continue
+    #     if line[:9] == '    <path':
+    #         for k in range(1,4):
+    #             if lines[l+k][7:12] == 'style':
+    #                 prop_line = lines[l+k]
+    #             elif lines[l+k][7] == 'd':
+    #                 path_line = lines[l+k]
+            
+    #         # Get color
+    #         color_idx_0 = prop_line.find('stroke:#')
+    #         color_idx_1 = prop_line[color_idx_0+8:].find(';')
+    #         opacity_idx_0 = prop_line.find('stroke-opacity:')
+    #         opacity_idx_1 = min( [prop_line[opacity_idx_0+15:].find('"'), prop_line[opacity_idx_0+15:].find(';')])
+    #         hex = prop_line[color_idx_0+8:color_idx_0+8+color_idx_1]
+    #         color = list(int(hex[i:i+2], 16)/255 for i in (0, 2, 4))
+    #         color.append(float(prop_line[opacity_idx_0+15:opacity_idx_0+15+opacity_idx_1]))
+    #         color = torch.tensor(color)
+    #         # Get width
+    #         width_idx_0 = prop_line.find('stroke-width:')
+    #         width_idx_1 = prop_line[width_idx_0+13:].find(';')
+    #         width = float(prop_line[width_idx_0+13:width_idx_0+13+width_idx_1])
+    #         width = torch.tensor(width)
+    #         # Get path
+    #         idx0 = path_line.find('m')
+    #         idx1 = path_line.find('c')
+    #         path = [[float(x) for x in path_line[idx0+2:idx1-1].split(',')]]
+    #         linedata = path_line[idx1+2:-2].split(' ')
+    #         for k in range(len(linedata)):
+    #             path.append([float(x) for x in linedata[k].split(',')])
+    #         num_segments = len(path)//3
+    #         path = torch.tensor(path)
+    #         v0 = torch.tensor([0,0])
+    #         for k in range(path.size(0)):
+    #             path[k,:] += v0
+    #             if k%3 == 0:
+    #                 v0 = path[k,:]
+                        
+    #         path_list.append(DrawingPath(path, color, width, num_segments))
+    # print(path, color, width, num_segments)
+
+    # dsasd
+    # return path_list
