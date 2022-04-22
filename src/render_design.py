a8e22e09d497b2643e101327609a60ca30acf29c
francisco ibarrola <fibarrola@sinc.unl.edu.ar>
2022-04-22 10:49:54 +1000

    removed unused load and save files

--- src/render_design.py
+++ src/render_design.py
@@ -1,6 +1,5 @@
 import pydiffvg
 import torch
-import pickle
 import random
 
 
@@ -64,73 +63,6 @@ class UserSketch:
         for group in self.shape_groups:
             group.stroke_color.requires_grad = True
             self.color_vars.append(group.stroke_color)
-
-
-def render_save_img(path_list, canvas_height, canvas_width):
-
-    # Initialize Curves
-    shapes = []
-    shape_groups = []
-
-    # First the ones from my drawing
-    for dpath in path_list:
-        num_control_points = torch.zeros(dpath.num_segments, dtype=torch.int32) + 2
-        points = torch.zeros_like(dpath.path)
-        stroke_width = dpath.width * 100
-        points[:, 0] = canvas_width * dpath.path[:, 0]
-        points[:, 1] = canvas_height * dpath.path[:, 1]
-        path = pydiffvg.Path(
-            num_control_points=num_control_points,
-            points=points,
-            stroke_width=stroke_width,
-            is_closed=False,
-        )
-        shapes.append(path)
-        path_group = pydiffvg.ShapeGroup(
-            shape_ids=torch.tensor([len(shapes) - 1]),
-            fill_color=None,
-            stroke_color=dpath.color,
-        )
-        shape_groups.append(path_group)
-
-    if not path_list:
-        img = torch.ones(
-            (1, 3, canvas_height, canvas_width),
-            device='cuda:0' if torch.cuda.is_available() else 'cpu',
-        )
-    else:
-        scene_args = pydiffvg.RenderFunction.serialize_scene(
-            canvas_width, canvas_height, shapes, shape_groups
-        )
-        render = pydiffvg.RenderFunction.apply
-        img = render(canvas_width, canvas_height, 2, 2, 0, None, *scene_args)
-        img = img[:, :, 3:4] * img[:, :, :3] + torch.ones(
-            img.shape[0], img.shape[1], 3, device=pydiffvg.get_device()
-        ) * (1 - img[:, :, 3:4])
-        img = img[:, :, :3].unsqueeze(0).permute(0, 3, 1, 2)
-
-    points_vars = []
-    stroke_width_vars = []
-    color_vars = []
-    for path in shapes:
-        path.points.requires_grad = True
-        points_vars.append(path.points)
-        path.stroke_width.requires_grad = True
-        stroke_width_vars.append(path.stroke_width)
-    for group in shape_groups:
-        group.stroke_color.requires_grad = True
-        color_vars.append(group.stroke_color)
-
-    with open('tmp/img0.pkl', 'wb') as f:
-        pickle.dump(img, f)
-    with open('tmp/points_vars.pkl', 'wb') as f:
-        pickle.dump(points_vars, f)
-    with open('tmp/stroke_width_vars.pkl', 'wb') as f:
-        pickle.dump(stroke_width_vars, f)
-    with open('tmp/color_vars.pkl', 'wb') as f:
-        pickle.dump(color_vars, f)
-
-    return shapes, shape_groups
 
 
 def treebranch_initialization(
@@ -228,19 +160,6 @@ def treebranch_initialization(
         shape_groups.append(path_group)
 
     return shapes, shape_groups
-
-
-def load_vars():
-    with open('tmp/points_vars.pkl', 'rb') as f:
-        points_vars0 = pickle.load(f)
-    with open('tmp/stroke_width_vars.pkl', 'rb') as f:
-        stroke_width_vars0 = pickle.load(f)
-    with open('tmp/color_vars.pkl', 'rb') as f:
-        color_vars0 = pickle.load(f)
-    with open('tmp/img0.pkl', 'rb') as f:
-        img0 = pickle.load(f)
-
-    return points_vars0, stroke_width_vars0, color_vars0, img0
 
 
 def add_shape_groups(a, b):
