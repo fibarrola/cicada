b7e7a540f48a3ee15c10a5f5d6cf2f2cdf4c0c0a
francisco ibarrola <fibarrola@sinc.unl.edu.ar>
2022-04-27 13:09:59 +1000

    pruning options

--- main.py
+++ main.py
@@ -16,12 +16,18 @@ save_path = Path("results/").joinpath(args.dir)
 save_path.mkdir(parents=True, exist_ok=True)
 save_path = str(save_path) + '/'
 
-drawing_model = DrawingModel(args, device)
-drawing_model.process_text(args)
-
 t0 = time.time()
 
+prune_places = [ round(args.num_iter*(k+1)*0.8/args.n_prunes) for k in range(args.n_prunes) ]
+p0 = args.prune_ratio
+
 for trial in range(args.num_trials):
+
+    args.prune_ratio = p0/len(prune_places)
+
+    drawing_model = DrawingModel(args, device)
+    drawing_model.process_text(args)
+
     time_str = (datetime.datetime.today() + datetime.timedelta(hours=11)).strftime(
         "%Y_%m_%d_%H_%M_%S"
     )
@@ -41,47 +47,20 @@ for trial in range(args.num_trials):
 
         drawing_model.run_epoch(t, args)
 
-        # Pruning
-        if args.prune_ratio > 0:
-            if t == round(args.num_iter * 0.8):
-                with torch.no_grad():
-                    pydiffvg.imwrite(
-                        drawing_model.img, save_path + time_str + '_preP.png', gamma=1,
-                    )
-                drawing_model.prune(args)
+        # Pruning       
+        if t in enumerate(prune_places):
+            with torch.no_grad():
+                pydiffvg.imwrite(
+                    drawing_model.img, save_path + time_str + f'_preP_{t}.png', gamma=1,
+                )
+            drawing_model.prune(args)
+            args.prune_ratio += p0/len(prune_places)
 
-            if t == round(args.num_iter * 0.8) + 1:
-                with torch.no_grad():
-                    pydiffvg.imwrite(
-                        drawing_model.img, save_path + time_str + '_postP.png', gamma=1,
-                    )
-
-        # Print stuff
-        # if t % 50 == 0:
-        #     print(f"Iteration {t}")
-        #     for loss_name in drawing_model.losses:
-        #         if loss_name == 'geometric':
-        #             for gl_name in drawing_model.losses[loss_name]:
-        #                 print(
-        #                     f'geo loss {gl_name}: {drawing_model.losses[loss_name][gl_name].item()}'
-        #                 )
-        #         else:
-        #             print(f"{loss_name}: {drawing_model.losses[loss_name].item()}")
-
-        #         im_norm = drawing_model.img_features / drawing_model.img_features.norm(
-        #             dim=-1, keepdim=True
-        #         )
-        #         noun_norm = (
-        #             drawing_model.nouns_features
-        #             / drawing_model.nouns_features.norm(dim=-1, keepdim=True)
-        #         )
-        #         similarity = (100.0 * im_norm @ noun_norm.T).softmax(dim=-1)
-        #         values, indices = similarity[0].topk(5)
-        #         print("\nTop predictions:\n")
-        #         for value, index in zip(values, indices):
-        #             print(
-        #                 f"{drawing_model.nouns[index]:>16s}: {100 * value.item():.2f}%"
-        #             )
+        if t-1 in prune_places:
+            with torch.no_grad():
+                pydiffvg.imwrite(
+                    drawing_model.img, save_path + time_str + f'_postP_{t-1}.png', gamma=1,
+                )
 
         utils.printProgressBar(
             t + 1, args.num_iter, drawing_model.losses['global'].item()
